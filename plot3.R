a39af677759a4ac8c9fdfb2f84871d1a8d35a40d
ravikeron <n.ravikeron@tcs.com>
2015-05-10 20:14:20 +0530

    updates to plot3 & 4 to fix the legend

--- plot3.R
+++ plot3.R
@@ -11,6 +11,6 @@ datetime <- strptime(paste(datasubset$Date, datasubset$Time, sep= " "), "%d/%m/%
 plot(datetime, as.numeric(datasubset$Sub_metering_1), type="l", xlab="", ylab="Energy Submetering")
 lines(datetime, as.numeric(datasubset$Sub_metering_2), type="l", col="red")
 lines(datetime, as.numeric(datasubset$Sub_metering_3), type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
--- plot4.R
+++ plot4.R
@@ -18,7 +18,7 @@ plot(datetime, as.numeric(datasubset$Voltage), type="l", xlab="datetime", ylab="
 plot(datetime, as.numeric(datasubset$Sub_metering_1), type="l", xlab="datetime", ylab="Energy Submetering")
 lines(datetime, as.numeric(datasubset$Sub_metering_2), type="l", col="red")
 lines(datetime, as.numeric(datasubset$Sub_metering_3), type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2, bty="n", col=c("black", "red", "blue"), cex=.75)
 
 plot(datetime, as.numeric(datasubset$Global_reactive_power), type="l", xlab="datetime", ylab="Global_reactive_power")
 
